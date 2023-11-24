f64cc0783edac81c18b7cab0b66cf12c62843723
Victor Vatn <victor.vatn@abbgymnasiet.se>
2023-11-24 13:18:16 +0100

    arduino now gets values from firebase

--- SmartCart/SmartCart.ino
+++ SmartCart/SmartCart.ino
@@ -1,4 +1,13 @@
 #include <Wire.h>
+#include "FirebaseESP8266.h" // Includerar ett biblotek som gör att ESP'n kan komunisera med FireBase
+#include <ESP8266WiFi.h> // Includerar ett biblotek som gör att ESP'n ska kunna använda WiFi
+
+#define FIREBASE_HOST "https://smart-cart-374c8-default-rtdb.europe-west1.firebasedatabase.app/" // Definerar var datan ska skickas
+#define FIREBASE_AUTH "WvYh2kcbqrnsQRiGs8lAe9QZTzNS2AZaR23v63F4" // Lösenord för att se att du är ägaren av FireBasen
+#define WIFI_SSID "ABB_Indgym_Guest" // Namnet på wifiet som esp'n ska använda
+#define WIFI_PASSWORD "Welcome2abb" // Lösenordet till WiFiet
+
+FirebaseData firebaseData1;
 
 const byte ENA = 13;
 const byte IN1 = 5;
@@ -19,20 +28,41 @@ int speedA = 0, speedB = 0;
 
 void setup() {
   Serial.begin(115200);
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  
   for (byte pin : PWMPins) pinMode(pin, OUTPUT);
   for (byte pin : dirPins) {
     pinMode(pin, OUTPUT);
     digitalWrite(pin, LOW);
   }
+
+  Serial.println();// printar en ny linje i serial monitorn
+  Serial.print("Connected with IP: "); // Printar att WiFi'et har connectat
+  Serial.println(WiFi.localIP()); // Skriver ut den locala IP adressen
+  Serial.println(); // Skriver ut en ny linje
+
+  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH); // Startar FireBase och skickar med var den ska skickas med HOST och AUTH för att veta att det är du som är ägare
+  Firebase.reconnectWiFi(true); // Reconnnectar WiFi'et med Firebase (kolla om det är det den gör och skriv bättre!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!)
+
+  delay(5000); // Väntar 5000 milisekunder eller 5 sekunder för att vara säker att fire base och wifi'et är kopplade innan vi börjar skicka information
+
 }
 
 int R = 1, L = -1;
 
 void loop() {
+  L = Firebase.getInt(firebaseData1, "/left");
+  R = Firebase.getInt(firebaseData1, "/right"); 
+
+  drive(L, R);
+  
+  /*
   drive(200, 200);
   delay(5000);
   drive(-200, -200);
-  delay(5000);
+  delay(5000);*/
+
+  
 }
 
 void drive(int mspeedA, int mspeedB){

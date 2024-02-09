8acc61dfcb29f0dbc1d3084b8fc5e1ba5a6eb424
Victor Vatn <victor.vatn@abbgymnasiet.se>
2024-02-09 15:30:17 +0100

    added kalman filter example

--- main.py
+++ main.py
@@ -1,10 +1,27 @@
 import cv2
 from ultralytics import YOLO
 import supervision as sv
+from filterpy.kalman import KalmanFilter
+from filterpy.common import Q_discrete_white_noise
+import numpy as np
+from copy import deepcopy
+
 
 # Load the YOLOv8 model
 model = YOLO('yolov8n.pt')
 
+kalmanfilters = []
+for i in range(4):
+    f = KalmanFilter(dim_x=2, dim_z=1)
+    f.x = np.array([2., 0.])
+    f.F = np.array([[1.,1.],
+                    [0.,1.]])
+    f.H = np.array([[1.,0.]])
+    f.P = np.array([[1000.,    0.],
+                    [   0., 1000.]])
+    f.R = 5
+    f.Q = Q_discrete_white_noise(dim=2, dt=0.1, var=0.13)
+    kalmanfilters.append(f)
 
 cap = cv2.VideoCapture(0)
 
@@ -16,28 +33,43 @@ while cap.isOpened():
     if success:
         # Run YOLOv8 inference on the frame
         results = model(frame)
-        detections = sv.Detections.from_ultralytics(results[0])
+        detections = sv.Detections.from_ultralytics(results[0]) # Detections class is not very nice to work with
         detections = detections[detections.class_id == 0]
 
-        detection = detections[0]
-        ydim, xdim = frame.shape[:2]
+        labels = ["person"] * len(detections.class_id)
+        if len(detections) > 0:
+            detection = detections[0]
+            ydim, xdim = frame.shape[:2]
 
-        boxleft = detection.xyxy[0][0]
-        boxright = detection.xyxy[0][2]
-        boxcenter = (boxleft + boxright) / 2
+            xyxy = detection.xyxy[0]
+            boxleft = detection.xyxy[0][0]
+            boxright = detection.xyxy[0][2]
+            boxcenter = (boxleft + boxright) / 2
 
-        if boxcenter < xdim / 2:
-            print("go left")
-        elif boxcenter > xdim / 2:
-            print("go right")
+            predictedxyxy = []
+            for i in range(4):
+                f = kalmanfilters[i]
+                f.predict()
+                predictedxyxy.append(f.x[0])
+                f.update(xyxy[i])
+            
+            predictedxyxy = np.array(predictedxyxy)
+            detections.xyxy = np.concatenate((detections.xyxy,[predictedxyxy]),axis=0)
+            detections.class_id = np.append(detections.class_id, 1)
+            labels.append("kalman")
 
+            if boxcenter < xdim / 2:
+                print("go left")
+            elif boxcenter > xdim / 2:
+                print("go right")
+
+        print(detections)
         box_annotator = sv.BoxAnnotator()
         annotated_frame = box_annotator.annotate(
             scene=frame.copy(),
             detections=detections,
-            labels=["person"]*len(detections.class_id)
+            labels=labels
         )
-                
 
         # Visualize the results on the frame
 
